d99e233c7a693dae360f16a82dee47b619c0b76e
nanaouyang <Gg3.1415926>
2020-08-23 18:15:24 +0800

    🚑

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,25 +3,36 @@ import Head from "next/head";
 import '@/styles/globals.scss'
 import styles from '@/styles/app.module.scss'
 import {Affix} from "antd";
+import Link from "next/link";
 
 
 function MyApp({Component, pageProps}) {
     return <>
         <Head>
             <title>zzf</title>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.ico"/>
         </Head>
         <Affix offsetTop={0}>
-            <header className={styles.header}>
-                <img src={'/static/img/img-3.png'} alt="logo" />
-                <nav>
+        <header className={styles.header}>
+            <img src={'/static/img/img-3.png'} alt="logo"/>
+            <nav>
+                <Link href="/">
                     <a>首页</a>
+                </Link>
+                <Link href="/archive">
                     <a>归档</a>
+                </Link>
+                <Link href="/tag">
                     <a>标签</a>
+                </Link>
+                <Link href="/changelog">
                     <a>更新日志</a>
+                </Link>
+                <Link href="/about">
                     <a>关于本站</a>
-                </nav>
-            </header>
+                </Link>
+            </nav>
+        </header>
         </Affix>
         <main className={styles.main}>
             <Component {...pageProps} />
