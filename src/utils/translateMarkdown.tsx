339c14ce56eaa657a62eda9a6b213e2a6ff2f0e0
chenc <chenc@fengyuntec.com>
2020-08-24 09:08:53 +0800

    fixed

--- src/utils/translateMarkdown.tsx
+++ src/utils/translateMarkdown.tsx
@@ -8,14 +8,14 @@ export const translateMarkdown = (text = '') => {
   const renderer = new marked.Renderer();
   renderer.code = function(code, language) {
     return `<pre><code class="language-${language}" lang=${language}>${
-      hljs.highlightAuto(code).value
+      language?hljs.highlight(language, code).value:hljs.highlightAuto(code).value
     }</code><a style="position: absolute;right: 10px;top: 10px;color:#8c8c8ccc;border:none">复制代码</a></pre>`;
   };
   renderer.heading = function (text, level) {
     return `<h${level} id=${encodeURIComponent(text)}>${text}</h${level}>`;
   }
   renderer.image = function (href,title,text) {
-    return `<div><img src=${href}  class="zoom" /></div>`;
+    return `<div><img src=${href}  class="zoom" alt=${title} /></div>`;
   }
   return marked(text, {
     renderer: renderer,
