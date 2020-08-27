70dc1cf193d89bd2bd220c48320b0a17acab54b1
chenc <chenc@fengyuntec.com>
2020-08-27 14:53:27 +0800

    添加备案号

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -87,6 +87,7 @@ function MyApp({ Component, pageProps }) {
       <footer className={styles.footer}>
         <div>2020</div>
         <div>Powered by Zzf</div>
+        <div>苏ICP备18059856号</div>
       </footer>
       {/*</Scrollbars>*/}
     </>
