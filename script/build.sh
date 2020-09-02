6130f5c93d9548569cb570c4dfd752e442831bb7
chenc <chenc@fengyuntec.com>
2020-09-02 17:19:02 +0800

    🚑

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,4 @@
-#! /bin/sh
-npm i
-rm -fr .next/*
-npm run build
-tar -zcvf dist.tar.gz ./.next
-tar -zcvf public.tar.gz ./public
+#!/usr/bin/env sh
+docker build -t 127.0.0.1:5000/react .
+docker push 127.0.0.1:5000/react:latest
+docker image prune -f
