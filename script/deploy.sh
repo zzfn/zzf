a8ea98f16ee8394f9a0124c60ea5e9d05ecb8deb
chenc <chenc@fengyuntec.com>
2020-09-07 14:10:43 +0800

    ✨

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
-cd /root/zzf
 
-container_name=zzf
-# docker部署
-docker pull registry.docker.annyyy.com/$container_name
-docker stop $container_name
-docker rm $container_name
-docker run --name=$container_name -d -p 9600:9600 registry.docker.annyyy.com/$container_name
-docker image prune -f
+#container_name=zzf
+## docker部署
+#docker pull registry.docker.annyyy.com/$container_name
+#docker stop $container_name
+#docker rm $container_name
+#docker run --name=$container_name -d -p 9600:9600 registry.docker.annyyy.com/$container_name
+#docker image prune -f
 
 #利用docker-compose部署
 #更新镜像
@@ -18,3 +17,9 @@ docker image prune -f
 #docker-compose up -d --force-recreate
 
 #docker image prune -f
+cd /www/wwwroot/zzfzzf.com
+git clone https://github.com/nanaouyang/zzf.git
+npm i
+npm run build
+pm2 delete zzf
+pm2 start npm --name "zzf" -- start
