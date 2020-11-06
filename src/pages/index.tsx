0ef4ab2c7705944a6bb40c2afc8b74152ee4f2da
chenc <chenc@fengyuntec.com>
2020-11-06 20:34:21 +0800

    feat: 添加文章搜索功能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "@/styles/home.module.scss";
-import ZImg from "com/common/zImg";
 import { listArticles } from "@/services/article";
 import ArticleCard from "com/article/articleCard";
-import { selectSysConfig } from "@/services/common";
+import { useDebounce } from "@/hooks/useDebounce";
 
 export default function Home(props): JSX.Element {
-  const { serverProps, bg } = props;
+  const { serverProps } = props;
   const [total, setTotal] = useState(serverProps.total);
+  const [val, setVal] = useState<string>();
   const [page, setPage] = useState(serverProps.page);
   const [records, setRecords] = useState(serverProps.records);
-
+  const de = useDebounce(change, 500);
+  function change(v) {
+    listArticles({ pageNumber: page, pageSize: 10, title: val }).then(
+      ({ data }) => {
+        setTotal(data.total);
+        setPage(data.current);
+        setRecords(data.records);
+      }
+    );
+  }
   useEffect(() => {
-    listArticles({ pageNumber: page, pageSize: 10 }).then(({ data }) => {
-      setTotal(data.total);
-      setPage(data.current);
-      setRecords(data.records);
-    });
-  }, [page]);
+    de();
+  }, [page, val]);
+
   return (
     <div className={styles.home}>
       <Head>
         <title>zzf~首页</title>
       </Head>
-      {/*<ZImg className={styles.bg} src={bg?.value} />*/}
+      <input
+        value={val}
+        onChange={(event) => setVal(event.target.value)}
+        type="text"
+      />
       {records.map((item: Article) => (
         <ArticleCard key={item.id} dataSource={item} />
       ))}
@@ -37,11 +47,9 @@ export const getServerSideProps = async () => {
   const num = 1;
   const size = 10;
   const { data } = await listArticles({ pageNumber: num, pageSize: size });
-  const { data: data0 } = await selectSysConfig({ field: "bg" });
   return {
     props: {
       serverProps: data,
-      bg: data0,
     },
   };
 };
