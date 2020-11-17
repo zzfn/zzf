ecd873dffe88a14ba0b03f08b76b5a24527c1d63
zzfn <chenc@fengyuntec.com>
2020-11-17 21:05:53 +0800

    feat: http请求封装修改

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,76 +1,35 @@
-import axios, { Method } from "axios";
-import baseUrl from "./index";
-
-/**
- * http封装请求
- * @param method
- * @param url
- * @param request
- */
-const http = async (
-  method: Method,
-  url: string,
-  request?: { data?: object; params?: object }
-): Promise<Res<any>> => {
-  try {
-    const res = await axios({
-      method,
-      url,
-      data: request ? request.data : null,
-      params: request ? request.params : null,
-      timeout: 120000,
-      baseURL: baseUrl,
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-      validateStatus: () => {
-        return true;
-      },
-    });
-    const { status, data } = res;
-    if (status === 200) {
-      const { code, message: msg } = data;
-      if (code !== 0) {
-      }
-      return data;
-    } else if (status === 404) {
-      return {
-        code: 4004,
-        data: null,
-        message: "api地址不存在",
-      };
-    } else {
-      return {
-        code: 4000,
-        data: null,
-        message: data.message,
-      };
-    }
-  } catch (e) {
-    return {
-      code: 5000,
-      data: null,
-      message: e.toString(),
-    };
-  }
-};
-axios.interceptors.request.use(
-  function (config) {
+import axios, { AxiosRequestConfig, Method } from "axios";
+import envConfig from "env";
+const instance = axios.create({
+  baseURL: envConfig.baseUrl,
+  timeout: 10000,
+  headers: { "Content-Type": "application/json;charset=UTF-8" },
+  validateStatus: function () {
+    return true;
+  },
+});
+instance.interceptors.request.use(
+  (config) => {
     return config;
   },
-  function (error) {
-    // Do something with request error
+  (error) => {
     return Promise.reject(error);
   }
 );
-axios.interceptors.response.use(
-  function (response) {
-    // Do something with response data
+instance.interceptors.response.use(
+  (response) => {
     return response;
   },
-  function (error) {
-    // Do something with response error
+  (error) => {
     return Promise.reject(error);
   }
 );
-export default http;
+
+export default async function http(
+  method: Method,
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<Res<any>> {
+  const { data } = await instance(url, { ...config, method });
+  return data;
+}
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-const baseUrl = (): string => {
-  return "https://api.zzfzzf.com";
-  // return process.env.NODE_ENV === "development"
-  //   ? "http://localhost:8060"
-  //   : "https://api.zzfzzf.com";
-};
-export default baseUrl();
-export const fileUrl: string = baseUrl() + "/oss/upload";
