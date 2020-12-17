d6afc05f28f3ef372a07e49dc4f42ae24db44485
chenc <chenc@fengyuntec.com>
2020-12-17 16:29:24 +0800

    feat: 进度条

--- src/components/article/Progress.tsx
+++ src/components/article/Progress.tsx
@@ -1,26 +1,23 @@
-import React, { useEffect, useRef } from "react";
-import styles from "./progress.module.scss";
+import React, { useEffect, useRef } from 'react';
+import styles from './progress.module.scss';
 
-function Progress(props) {
+function Progress(): JSX.Element {
   const ins = useRef<any>();
 
   function watch() {
     const winHeight = window.innerHeight;
     const docHeight = document.documentElement.scrollHeight;
-    console.log(ins.current.value);
-    console.log(winHeight);
-    console.log(docHeight);
     ins.current.max = docHeight - winHeight;
     ins.current.value = window.scrollY;
   }
 
   useEffect(() => {
-    document.addEventListener("scroll", watch);
+    document.addEventListener('scroll', watch);
     return () => {
-      document.removeEventListener("scroll", watch);
+      document.removeEventListener('scroll', watch);
     };
   }, []);
-  return <progress className={styles.progress} ref={ins} value="0" />;
+  return <progress className={styles.progress} ref={ins} value='0' />;
 }
 
 export default Progress;
