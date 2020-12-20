c851166e659da0a9bbf38fc104f80c0b7432587e
zzfn <chenc@fengyuntec.com>
2020-12-20 14:26:51 +0800

    feat: 关于页面美化

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,13 +8,16 @@ export default function About({ serverProps }): JSX.Element {
       <Head>
         <title>关于~zzf</title>
       </Head>
-      <ul>
-        <li>博客前端采用react+nextjs完成,无ui框架,逐步减少依赖,手动实现各个依赖</li>
-        <li>后端采用springboot+MyBatis-Plus完成，数据库采用mysql,缓存使用redis</li>
-        <li>支持跟随系统深色与浅色模式</li>
-        <li>2020.12.10--文章搜索功能集成Elasticsearch7.10.0</li>
-        <li>2020.12.19--后端采用微服务架构</li>
-      </ul>
+      <header>zzfn 上次修改2020.12.20</header>
+      <article>
+        <ul>
+          <li>博客前端采用react+nextjs完成,无ui框架,逐步减少依赖,手动实现各个依赖</li>
+          <li>后端采用springboot+MyBatis-Plus完成，数据库采用mysql,缓存使用redis</li>
+          <li>支持跟随系统深色与浅色模式</li>
+          <li>2020.12.10--文章搜索功能集成Elasticsearch7.10.0</li>
+          <li>2020.12.19--后端采用微服务架构</li>
+        </ul>
+      </article>
     </div>
   );
 }
