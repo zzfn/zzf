1395fb547b4ae2e6f5bfef1860d5c9137c62131f
chenc <chenc@fengyuntec.com>
2020-12-21 14:33:41 +0800

    feat: 禁止缩放

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,10 @@ class MyDocument extends Document {
             name='keywords'
             content='前端博客,个人博客,javascript,vue,react,正则表达式,webpack,docker'
           />
+          <meta
+            content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;'
+            name='viewport'
+          />
           <meta name='description' content='zzf的个人网站,记录个人学习' />
           <meta name='theme-color' content='#ffffff' />
         </Head>
