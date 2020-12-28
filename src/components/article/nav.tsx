6d1ca51a6950ecdd7059f62f7a7ce732fd27bf8d
zzfn <chenc@fengyuntec.com>
2020-12-28 20:14:49 +0800

    feat: 导航

--- src/components/article/nav.tsx
+++ src/components/article/nav.tsx
@@ -7,28 +7,33 @@ const Nav: React.FC<NavProps> = ({ source }) => {
   const [list, setList] = useState([]);
   useEffect(() => {
     const matchResult = getTitle(source);
-    const navData = matchResult.map((r, i) => ({
-      index: i,
-      level: r.match(/^#+/g)[0].length,
-      text: r.replace(/#+\s(\S+)\n*/g, '$1'),
-    }));
-    setList(navData);
-    const zxxObserver = new IntersectionObserver(function (entries) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          console.log(entry.target.getAttribute('data-id'));
-        } else {
-          console.log(entry.target.getAttribute('data-id'));
+    if (matchResult) {
+      const navData = matchResult.map((r, i) => ({
+        index: i,
+        level: r.match(/^#+/g)[0].length,
+        text: r.replace(/#+\s(\S+)\n*/g, '$1'),
+      }));
+      setList(navData);
+      const zxxObserver = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            console.log(entry.target.getAttribute('data-id'));
+          } else {
+            console.log(entry.target.getAttribute('data-id'));
+          }
+        });
+      });
+      // 观察标题元素
+      document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function (ele, idx) {
+        if (idx) {
+          ele.setAttribute('data-id', 'heading-' + (idx - 1));
+          zxxObserver.observe(ele);
         }
       });
-    });
-    // 观察标题元素
-    document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function (ele, idx) {
-      if (idx) {
-        ele.setAttribute('data-id', 'heading-' + (idx - 1));
-        zxxObserver.observe(ele);
-      }
-    });
+      const hash = window.location.hash;
+      const e = document.querySelector(`[data-id = ${hash.replace('#', '')}]`);
+      e.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
   }, []);
   return (
     <ul style={{ position: 'fixed', top: '80px' }}>
@@ -37,7 +42,6 @@ const Nav: React.FC<NavProps> = ({ source }) => {
           onClick={() => {
             window.location.hash = `heading-${nav.index}`;
             const e = document.querySelector(`[data-id = heading-${nav.index}]`);
-            console.log(e);
             e.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
           }}
           style={{ marginLeft: `${nav.level * 10}px`, border: '1px solid #ccc' }}
