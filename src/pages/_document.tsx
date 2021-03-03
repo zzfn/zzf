584fa4a6d5a85bdcbf3f82b728637730cf123502
chenc <chenc@fengyuntec.com>
2021-03-03 15:58:36 +0800

    feat: 添加百度统计

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
 
 class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {...initialProps};
+    }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  render() {
-    return (
-      <Html lang='zh'>
-        <Head>
-          <link rel='icon' href='/static/favicon.ico' />
-          <meta
-            name='keywords'
-            content='前端博客,个人博客,javascript,vue,react,正则表达式,webpack,docker'
-          />
-          <meta name='description' content='zzf的个人网站,记录个人学习' />
-          <meta name='theme-color' content='#ffffff' />
-        </Head>
-        <body className={'light'}>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    render() {
+        return (
+            <Html lang='zh'>
+                <Head>
+                    <link rel='icon' href='/static/favicon.ico'/>
+                    <meta
+                        name='keywords'
+                        content='前端博客,个人博客,javascript,vue,react,正则表达式,webpack,docker'
+                    />
+                    <meta name='description' content='zzf的个人网站,记录个人学习'/>
+                    <meta name='theme-color' content='#ffffff'/>
+                    <script>
+                        var _hmt = _hmt || [];
+                        (function() {
+                        var hm = document.createElement("script");
+                        hm.src = "https://hm.baidu.com/hm.js?ed2dea228a6c252969d46cf1aad258a6";
+                        var s = document.getElementsByTagName("script")[0];
+                        s.parentNode.insertBefore(hm, s);
+                    })();
+                    </script>
+                </Head>
+                <body className={'light'}>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
