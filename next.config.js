7e5709e12e52f8496198713a73f47b02cdce1952
chenc <chenchen3@fosun.com>
2021-04-30 13:50:46 +0800

    chore: 升级nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 module.exports = {
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true
+  },
   webpack: (config, { isServer }) => {
     return config;
   },
