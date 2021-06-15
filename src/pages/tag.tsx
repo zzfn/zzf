619f398f7c18b25d6f0fdaf8749242c9fb62ec42
chenc <chenchen3@fosun.com>
2021-06-15 18:26:14 +0800

    标签页修改

--- src/pages/tag.tsx
+++ src/pages/tag.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import styles from 'styles/tag.module.scss';
 import Head from 'next/head';
 import { listTags } from 'services/article';
 import Link from 'next/link';
 import Arrow from 'components/arrow/arrow';
 import { getTitle } from '../utils/getTitle';
 
-export default function TagPage({ serverProps }): JSX.Element {
+export default function TagPage({ serverProps }: NextProps<any>): JSX.Element {
   return (
-    <div className={styles.tagWrap}>
+    <>
       <Head>
         <title>{getTitle('标签')}</title>
       </Head>
       {serverProps.map((item) => (
-        <div key={item.code} style={{ margin: '10px', display: 'inline-block' }}>
+        <div key={item.code} className={'m-2 inline-block'}>
           <Link href={`/tag/${item.code}?desc=${encodeURIComponent(item.tag)}`}>
             <a>
               <Arrow color={'#108ee9'} number={item.count}>
@@ -23,7 +22,7 @@ export default function TagPage({ serverProps }): JSX.Element {
           </Link>
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
