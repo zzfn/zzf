2259bc9269c1f289a692204b3295e5e494239f58
chenc <git@zzfzzf.com>
2021-06-20 02:36:22 +0800

    feat: 手机端兼容模式切换

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -75,16 +75,58 @@ function Header(): JSX.Element {
           </>
         ) : (
           <>
-            <Link href={'/'}>
-              <a className={'flex items-center color-text-primary'}>
-                <Icon className={'color-text-primary text-6xl'} name={'logo'} />
-              </a>
-            </Link>
             <Icon
               className={'color-text-primary text-4xl'}
               onClick={() => setIsShow(!isShow)}
               name={isShow ? 'close' : 'menu'}
             />
+            <Link href={'/'}>
+              <a className={'flex items-center color-text-primary'}>
+                <Icon className={'color-text-primary text-6xl'} name={'logo'} />
+              </a>
+            </Link>
+            <details className='dropdown details-reset details-overlay d-inline-block'>
+              <summary aria-haspopup='true'>
+                <Icon className={'color-text-primary text-4xl'} name={'setting'} />
+              </summary>
+              <ul className='dropdown-menu dropdown-menu-sw'>
+                <li
+                  onClick={() => {
+                    document.querySelector('html').setAttribute('data-color-mode', 'dark');
+                    document.querySelector('html').setAttribute('data-dark-theme', 'dark_dimmed');
+                    localStorage.setItem('data-color-mode', 'dark');
+                    localStorage.setItem('data-dark-theme', 'dark_dimmed');
+                  }}
+                  className={'dropdown-item'}
+                >
+                  深色
+                </li>
+                <li
+                  onClick={() => {
+                    document.querySelector('html').setAttribute('data-color-mode', 'light');
+                    document.querySelector('html').setAttribute('data-dark-theme', 'light');
+                    localStorage.setItem('data-color-mode', 'light');
+                    localStorage.setItem('data-light-theme', 'light');
+                  }}
+                  className={'dropdown-item'}
+                >
+                  浅色
+                </li>
+                <li
+                  onClick={() => {
+                    document.querySelector('html').setAttribute('data-color-mode', 'auto');
+                    document.querySelector('html').setAttribute('data-light-theme', 'light');
+                    document.querySelector('html').setAttribute('data-dark-theme', 'dark_dimmed');
+                    localStorage.setItem('data-color-mode', 'auto');
+                    localStorage.setItem('data-light-theme', 'light');
+                    localStorage.setItem('data-dark-theme', 'dark_dimmed');
+                  }}
+                  className={'dropdown-item'}
+                >
+                  跟随系统
+                </li>
+              </ul>
+            </details>
           </>
         )}
       </Layout.Header>
@@ -95,55 +137,6 @@ function Header(): JSX.Element {
               {menu.name}
             </Link>
           ))}
-          <details className='dropdown details-reset details-overlay d-inline-block'>
-            <summary
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-              className='btn'
-              aria-haspopup='true'
-            >
-              主题
-              <div className='dropdown-caret' />
-            </summary>
-            <ul className='dropdown-menu dropdown-menu-se'>
-              <li
-                onClick={() => {
-                  document.querySelector('html').setAttribute('data-color-mode', 'dark');
-                  document.querySelector('html').setAttribute('data-dark-theme', 'dark_dimmed');
-                  localStorage.setItem('data-color-mode', 'dark');
-                  localStorage.setItem('data-dark-theme', 'dark_dimmed');
-                }}
-                className={'dropdown-item'}
-              >
-                深色
-              </li>
-              <li
-                onClick={() => {
-                  document.querySelector('html').setAttribute('data-color-mode', 'light');
-                  document.querySelector('html').setAttribute('data-dark-theme', 'light');
-                  localStorage.setItem('data-color-mode', 'light');
-                  localStorage.setItem('data-light-theme', 'light');
-                }}
-                className={'dropdown-item'}
-              >
-                浅色
-              </li>
-              <li
-                onClick={() => {
-                  document.querySelector('html').setAttribute('data-color-mode', 'auto');
-                  document.querySelector('html').setAttribute('data-light-theme', 'light');
-                  document.querySelector('html').setAttribute('data-dark-theme', 'dark_dimmed');
-                  localStorage.setItem('data-color-mode', 'auto');
-                  localStorage.setItem('data-light-theme', 'light');
-                  localStorage.setItem('data-dark-theme', 'dark_dimmed');
-                }}
-                className={'dropdown-item'}
-              >
-                跟随系统
-              </li>
-            </ul>
-          </details>
         </nav>
       )}
     </>
