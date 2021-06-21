3fcd402d1cc0b806e89ba6ffba6f99aed8268c24
chenc <chenchen3@fosun.com>
2021-06-21 15:06:46 +0800

    feat: react17

--- src/utils/translateMarkdown.tsx
+++ src/utils/translateMarkdown.tsx
@@ -1,16 +1,17 @@
 import marked from 'marked';
 import { renderToString } from 'react-dom/server';
 import Code from 'components/article/code';
+``;
 export const translateMarkdown = (text = '') => {
   const renderer = new marked.Renderer();
   renderer.code = function (code, language) {
     return renderToString(<Code language={language} code={code} />);
   };
   renderer.image = function (href, title, text) {
-    return `<img loading="lazy" src=${href}  class="zoom" alt=${text} />`;
+    return `<img loading='lazy' src='${href}'  class='zoom' alt='${text}' />`;
   };
   renderer.link = function (href, title, text) {
-    return `<a target="_blank" href=${href}>${text}</a>`;
+    return `<a target='_blank' href='${href}'>${text}</a>`;
   };
   renderer.heading = function (text, level, _, slugger) {
     const id = slugger.slug('heading');
