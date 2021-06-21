a5a5130ae93d4784afacfa9f40d37fa331007ff0
chenc <chenchen3@fosun.com>
2021-06-21 15:00:43 +0800

    feat: nav修复

--- src/components/article/nav.tsx
+++ src/components/article/nav.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Markdown } from '@zzf/toolkit';
 import styles from './nav.module.scss';
+import classNames from 'classnames';
+
 interface NavProps {
   source: string;
 }
+
 const Nav: React.FC<NavProps> = ({ source }) => {
   const [list, setList] = useState([]);
   const [current, setCurrent] = useState('');
   useEffect(() => {
-    window.onhashchange = function () {
-      setCurrent(window.location.hash.replace('#', ''));
-    };
+    setCurrent(window.location.hash.replace('#', ''));
     const matchResult = Markdown.getTitle(source);
     if (matchResult) {
       const navData = matchResult.map((r, i) => ({
@@ -19,43 +20,36 @@ const Nav: React.FC<NavProps> = ({ source }) => {
         text: r.replace(/#+\s(\S+)\n*/g, '$1'),
       }));
       setList(navData);
-      const zxxObserver = new IntersectionObserver(function (entries) {
-        entries.reverse().forEach(function (entry) {
-          if (entry.isIntersecting) {
-            setCurrent(entry.target.getAttribute('data-id'));
-          }
-        });
-      });
-      // 观察标题元素
-      document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function (ele, idx) {
-        if (idx) {
-          ele.setAttribute('data-id', 'heading-' + (idx - 1));
-          zxxObserver.observe(ele);
+    }
+    const navObserver = new IntersectionObserver(function (entries) {
+      entries.reverse().forEach(function (entry) {
+        if (entry.isIntersecting) {
+          setCurrent(entry.target.getAttribute('id'));
         }
       });
-      const hash = window.location.hash;
-      if (hash) {
-        const e = document.querySelector(`[data-id = ${hash.replace('#', '')}]`);
-        e && e.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
-        setCurrent(hash.replace('#', ''));
-      }
-    }
-  }, []);
+    });
+    document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function (ele) {
+      navObserver.observe(ele);
+    });
+  }, [source]);
   return (
     <ul className={styles.nav}>
       {list.map((nav) => (
         <li
           onClick={() => {
-            window.location.hash = `heading-${nav.index}`;
-            const e = document.querySelector(`[data-id = heading-${nav.index}]`);
-            e.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
             setCurrent(`heading-${nav.index}`);
           }}
-          className={`${styles.navItem} ${current === 'heading-' + nav.index ? styles.active : ''}`}
+          className={classNames(
+            styles.navItem,
+            {
+              [styles.active]: current === `heading-${nav.index}`,
+            },
+            'truncate',
+          )}
           style={{ marginLeft: `${(nav.level - 1) * 10}px` }}
           key={nav.index}
         >
-          {nav.text}
+          <a href={`#heading-${nav.index}`}>{nav.text}</a>
         </li>
       ))}
     </ul>
