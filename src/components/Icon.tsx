935300a5c6c885aaa35d8fc7360563056e862e66
chenc <chenchen3@fosun.com>
2021-06-21 10:38:16 +0800

    chore: fix

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const request = require.context('../assets', false, /\.svg$/);
-request.keys().forEach(request);
+// const request = require.context('../assets', false, /\.svg$/);
+// request.keys().forEach(request);
 type IconProps = {
   name: string;
   onClick?: () => void;
