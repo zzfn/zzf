07dbd82c5bec1d2cd3859e1c9a9f7a8f0a479121
chenc <git@zzfzzf.com>
2021-07-09 22:37:23 +0800

    环境变量

--- env/development.env.js
+++ env/development.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NEXT_PUBLIC_baseUrl: 'http://localhost:8060',
+  NEXT_PUBLIC_BASE_URL: 'http://localhost:8060',
+  NEXT_PUBLIC_NODE_URL: 'http://localhost:3000',
 };
--- env/production.env.js
+++ env/production.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NEXT_PUBLIC_baseUrl: 'https://api.zzfzzf.com',
+  NEXT_PUBLIC_BASE_URL: 'https://api.zzfzzf.com',
+  NEXT_PUBLIC_NODE_URL: 'https://service.zzfzzf.com',
 };
