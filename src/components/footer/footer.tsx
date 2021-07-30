6bf15f8b57b0a0f7102dfae050c158ee8ca6f7c5
chenc <chenchen3@fosun.com>
2021-07-30 14:33:42 +0800

    feat: 优化

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import styles from './footer.module.scss';
 import dayjs from 'dayjs';
 import { Layout } from '@zzf/design';
+import classNames from 'classnames';
 
 function Footer(): JSX.Element {
   return (
-    <Layout.Footer className={styles.footer}>
+    <Layout.Footer className={classNames('flex', 'flex-col', 'items-center', 'py-8', 'leading-6')}>
       <div>2020-{dayjs().format('YYYY')}</div>
       <div>Powered by zzfn</div>
       <div>苏ICP备18059856号</div>
-      <div>email: admin@annyyy.com</div>
     </Layout.Footer>
   );
 }
