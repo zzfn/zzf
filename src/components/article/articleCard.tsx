5daf5c28a09af8fd19880fd2ea3f2e638c434953
chenc <chenchen3@fosun.com>
2021-08-26 23:21:54 +0800

    feat: 主题色变换

--- src/components/article/articleCard.tsx
+++ src/components/article/articleCard.tsx
@@ -17,7 +17,12 @@ export default function ArticleCard({ dataSource }: ArticleCardProps): JSX.Eleme
       <div className={'flex items-center'}>
         <Link prefetch={false} href={`/article/${dataSource.id}`}>
           <a
-            className={classNames('color-text-link', 'text-2xl', 'font-semibold')}
+            className={classNames(
+              'text-primary4',
+              'hover:text-primary5',
+              'text-2xl',
+              'font-semibold',
+            )}
             target={'_blank'}
           >
             {dataSource.title}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     colors: {
       primary: 'var(--color-primary)',
       colorBorderSecondary: 'var(--color-border-secondary)',
+      primary4: 'var(--color-scale-orange-4)',
+      primary5: 'var(--color-scale-orange-5)',
       secondary: '#ffed4a',
       danger: '#e3342f',
     },
