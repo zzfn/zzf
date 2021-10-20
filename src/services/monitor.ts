ed6637eddde57736e22cb4cb8ffd6ad0055b6ae5
chenc <chenchen3@fosun.com>
2021-10-20 13:58:44 +0800

    chore: 更换url

--- src/services/monitor.ts
+++ src/services/monitor.ts
@@ -2,9 +2,8 @@ import http from 'utils/http';
 
 export const monitorLoad = (data: any) => {
   return http({
-    baseURL: process.env.NEXT_PUBLIC_NODE_URL,
     method: 'post',
-    url: '/monitor/trace',
+    url: 'trace/save',
     data,
   });
 };
