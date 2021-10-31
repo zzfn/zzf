ddf3fb68e56126526ef81d9d3c8a5b8444e4a15c
cc <cc@zzfzzf.com>
2021-10-31 20:32:47 +0800

    fix: axios封装修改

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,28 +1,32 @@
-import type { AxiosRequestConfig } from 'axios';
+import type { AxiosPromise, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  timeout: 10000,
-  headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-  validateStatus: function () {
-    return true;
-  },
-});
-instance.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-instance.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-export default instance as (options: AxiosRequestConfig) => Promise<any>;
+function http(config: AxiosRequestConfig): AxiosPromise {
+  const instance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    timeout: 10000,
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+    validateStatus: function () {
+      return true;
+    },
+  });
+  instance.interceptors.request.use(
+    (config) => {
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+  instance.interceptors.response.use(
+    (response) => {
+      return response.data;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+  return instance(config);
+}
+
+export default http;
