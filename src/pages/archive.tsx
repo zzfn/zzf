1a9a487ba2d0bc101c29b1b9795311e0b34fb558
zzfn <44751363+zzfn@users.noreply.github.com>
2022-01-24 11:03:52 +0800

    feat: style

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -44,7 +44,7 @@ function renderMonth(time: string, list: ListProps[]) {
             )}
             key={item.id}
           >
-            <span style={{ color: '#8a8a8a', marginRight: '10px', fontFamily: 'Helvetica Neue' }}>
+            <span className={classNames('font-mono', 'text-info','mr-3')}>
               {dayjs(item.createTime).format('YYYY-MM-DD')}
             </span>
             <Link href={`/article/${item.id}`}>
