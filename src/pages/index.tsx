f22d29b3649f88967f3b76aba53041a62ee1cd62
zzfn <44751363+zzfn@users.noreply.github.com>
2022-02-07 16:05:25 +0800

    feat: eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Head from 'next/head';
 import { lastUpdated, listArticles } from 'api/article';
 import { Card, Layout, Loading, SvgIcon } from '@zzf/design';
