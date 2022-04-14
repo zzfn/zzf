3d6d9bfb16d7e0941052491d3d99eb0f539400df
cc <44751363+zzfn@users.noreply.github.com>
2022-04-14 17:36:19 +0800

    feat: update

--- src/components/Evaluation/index.tsx
+++ src/components/Evaluation/index.tsx
@@ -6,6 +6,7 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs';
 function Evaluation(props: any) {
   const { id } = props;
   const [list, setList] = useState([]);
+  const [msg, setMsg] = useState('');
   const initial = async () => {
     const { data } = await listDiscuss({ id });
     setList(data);
@@ -14,11 +15,12 @@ function Evaluation(props: any) {
     const instance = await FingerprintJS.load();
     const result = await instance.get();
     const { data } = await saveDiscuss({
-      articleId: '1414224246711787522',
-      content: 'asd12312321312asdasd',
+      articleId: id,
+      content: msg,
       createBy: result.visitorId,
     });
     // console.log(data);
+    initial()
   };
   useEffect(() => {
     initial();
@@ -29,8 +31,13 @@ function Evaluation(props: any) {
       <header>
         <span>全部评论（{list?.length}）</span>
         <span onClick={save}>评论</span>
-        <input type='text' />
       </header>
+      <div>
+        <textarea value={msg} onChange={(e) => setMsg(e.target.value)} className='border' />
+        <button onClick={save} className='border'>
+          评论
+        </button>
+      </div>
       {list?.map((item: any, index: number) => {
         return <Card index={index} key={item.id} data={item} />;
       })}
