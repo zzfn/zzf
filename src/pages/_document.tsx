cf6fab82c722744ec94ada773af5702c9e01bf54
cc <44751363+zzfn@users.noreply.github.com>
2022-04-19 14:38:27 +0800

    feat: up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
-      <Main />
-      <div id='modal-root' />
-      <NextScript />
+        <Main />
+        <div id='modal-root' />
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
