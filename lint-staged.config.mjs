5e9c67b419277017280e6381f3fa9a6e3384446a
cc <44751363+zzfn@users.noreply.github.com>
2022-04-20 14:38:35 +0800

    feat: lint-stage

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
-module.exports = {
-  '*.{js,ts,tsx}': 'next lint --fix src',
+export default {
+  '*.{js,ts,tsx}': 'next lint --fix',
   '*.(scss|css)': 'stylelint --fix',
   '*.{js,ts,tsx,scss,css}': 'prettier --write',
 };
