113fce0e6a2eb3c7b1181b8866229edd4bc1c424
zzfn <44751363+zzfn@users.noreply.github.com>
2022-08-09 11:05:17 +0800

    feat: beian

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,8 @@ import classNames from 'classnames';
 function Footer(): JSX.Element {
   return (
     <div className={classNames('text-gray-700', 'text-xs', 'text-center', 'mt-6')}>
-      Powered by zzfn 2020-{dayjs().format('YYYY')}&copy;苏ICP备18059856号
+      Powered by zzfn 2020-{dayjs().format('YYYY')}&copy;
+      <a href='//beian.miit.gov.cn'>苏ICP备18059856号</a>
     </div>
   );
 }
