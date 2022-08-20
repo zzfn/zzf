4268c7936d7e2e9f76f09f93c4bc2c70251b6312
cc <44751363+zzfn@users.noreply.github.com>
2022-08-20 09:47:58 +0800

    feat: eslint

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,12 @@
+import path from 'path';
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 export default {
-  '*.{js,ts,tsx}': ['eslint --cache --cache-location .next/cache/eslint/ --fix'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
   '*.(scss|css)': 'stylelint --fix',
   '*.{js,ts,tsx,scss,css}': 'prettier --write',
 };
