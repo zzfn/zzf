633f750d18cb93aa0a3d902f1e7e86ec24155130
cc <44751363+zzfn@users.noreply.github.com>
2022-09-17 18:07:28 +0800

    feat: zx

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 export default {
-    swcMinify: false,
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://oss-zzf.zzfzzf.com/zzf' : '/',
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-        formats: ['image/avif', 'image/webp'],
-        domains: ['oss-zzf.zzfzzf.com','www.dmoe.cc'],
-    },
-    sassOptions: {
-        prependData: `@import "styles/variable";`,
-    },
+  swcMinify: false,
+  assetPrefix:
+    process.env.NODE_ENV === 'production' && !process.env.VERCEL
+      ? 'https://oss-zzf.zzfzzf.com/zzf'
+      : '/',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
+    domains: ['oss-zzf.zzfzzf.com', 'www.dmoe.cc'],
+  },
+  sassOptions: {
+    prependData: `@import "styles/variable";`,
+  },
 };
