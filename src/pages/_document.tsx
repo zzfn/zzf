554e3d36a5f8ff47e761e7870fd6a3bc7480b505
orluna <44751363+zzfn@users.noreply.github.com>
2022-10-17 09:49:24 +0800

    feat: 字体

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/lxgw-wenkai-lite-webfont@1.1.0/style.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
