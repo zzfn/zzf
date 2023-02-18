4a8be3fa70c1fa72f5f3eea97e2b1d2e7754b6ab
orluna <44751363+zzfn@users.noreply.github.com>
2023-02-18 15:07:15 +0800

    feat: diff

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,7 +1,11 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import locale_cn from 'dayjs/locale/zh-cn';
 dayjs.extend(relativeTime);
+dayjs.extend(utc)
+dayjs.extend(timezone)
 export function diff(time: string) {
-  return dayjs(time).locale(locale_cn).fromNow();
+  return dayjs.tz(time,'Asia/Shanghai').utc().locale(locale_cn).fromNow()
 }
