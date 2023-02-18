25484082f3d6e280cc761bc83dc696ca1f6c1679
orluna <44751363+zzfn@users.noreply.github.com>
2023-02-18 23:03:06 +0800

    feat: icon

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -2,7 +2,6 @@ import { Layout } from '@oc/design';
 import Header from './header';
 import Footer from './footer';
 import type { ReactNode } from 'react';
-import Aside from './Aside';
 
 function DefaultLayout({ children }: { children: ReactNode }) {
   return (
