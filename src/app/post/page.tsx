714c8bfe9b3b37f90af753f1d6d225d17b7048e1
cw <44751363+zzfn@users.noreply.github.com>
2023-08-18 22:45:32 +0800

    feat: post页面样式

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { listArchives, listTags } from "api/article";
-import dayjs from "dayjs";
+import { listArchives } from 'api/article';
+import dayjs from 'dayjs';
 
 export default async function Page() {
-  const { data=[] } = await listArchives({ });
+  const { data = [] } = await listArchives({});
   return (
-    <>
-      <div className='flex flex-col gap-y-2'>
+    <div className='mx-auto max-w-3xl'>
+      <h3 className='mt-6 text-muted'>当前共有 {data.length} 篇文章，加油！</h3>
+      <div className='flex flex-col text-sm p-6'>
         {data.map((post: any) => (
-          <a key={post.id} href={`/post/${post.id}`} className='flex justify-between hover:bg-muted p-2 rounded'>
+          <a
+            key={post.id}
+            href={`/post/${post.id}`}
+            className='flex items-center justify-between hover:bg-muted py-1 timeline rounded'
+          >
             <span>{post.title}</span>
-            <time className="font-mono font-thin text-muted">{dayjs(post.createTime).format('YYYY-MM-DD')}</time>
+            <time className='font-mono font-thin text-muted'>
+              {dayjs(post.createTime).format('YYYY-MM-DD')}
+            </time>
           </a>
         ))}
       </div>
-    </>
+    </div>
   );
 }
