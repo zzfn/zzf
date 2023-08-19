605844bf13383fe8e3ba1d50e81f3c822b94575d
cw <44751363+zzfn@users.noreply.github.com>
2023-08-19 11:06:43 +0800

    feat: 首页样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,45 @@ import React from 'react';
 import IconSymbols from '../components/IconSymbols';
 import Link from 'next/link';
 import dayjs from 'dayjs';
-import { sortByField } from "api/article";
+import { sortByField } from 'api/article';
+import { diff } from '../utils/time';
+import { Tooltip } from '@oc/design';
 
 export default async function Page() {
-  const { data } = await sortByField({field:'updateTime'});
+  const { data } = await sortByField({ field: 'createTime' });
   return (
-    <>
-      <h3 className='my-8 text-2.5xl flex justify-between items-center'>
-        <span>Recent Posts</span>
-        <Link href='/'>
-          <IconSymbols icon='all_inclusive' />
-        </Link>
-      </h3>
-      <div className='flex flex-col gap-y-2'>
-        {data.map((post: any) => (
-          <a
-            key={post.id}
-            href={`/post/${post.id}`}
-            className='flex justify-between hover:bg-muted p-2 rounded'
-          >
-            <span>{post.title}</span>
-            <time className='font-mono font-thin text-muted'>
-              {dayjs(post.createTime).format('YYYY-MM-DD')}
-            </time>
-          </a>
-        ))}
+    <div className='py-6'>
+      <div className='grid grid-cols-2'>
+        <div className='text-xl flex justify-center items-center flex-col gap-y-2'>
+          <p>看看最近我都写了些什么，</p>
+          <p>或许你会发现一些有趣的东西。</p>
+        </div>
+        <div className='flex flex-col gap-y-2'>
+          {data.map((post: any) => (
+            <a
+              key={post.id}
+              href={`/post/${post.id}`}
+              className='hover:bg-muted p-3 rounded-xl border border-default hover:border-primary flex flex-col gap-y-1'
+            >
+              <div>{post.title}</div>
+              <div className='flex items-center gap-x-2 text-xs text-muted'>
+                <time className='font-mono'>{diff(post.createTime)}</time>
+                {post.createTime !== post.updateTime && (
+                  <Tooltip content={post.updateTime}>
+                    <span>（已编辑）</span>
+                  </Tooltip>
+                )}
+                <span># {post.tag}</span>
+              </div>
+            </a>
+          ))}
+          <Link href='/post'>
+            <p className='text-center text-muted cursor-pointer hover:underline'>
+              还有更多，去查看
+            </p>
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
