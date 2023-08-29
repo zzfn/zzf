449068b53bf5d9d1c05196878c29565df447318a
cw <44751363+zzfn@users.noreply.github.com>
2023-08-29 12:38:10 +0800

    style: 一些动画

--- src/components/ArticleNav.tsx
+++ src/components/ArticleNav.tsx
@@ -1,18 +1,27 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getTitle } from 'utils/translateMarkdown';
 import classNames from 'classnames';
-import { useMotionValueEvent, useScroll, useSpring } from 'framer-motion';
+import { AnimatePresence, useMotionValueEvent, useScroll, motion } from 'framer-motion';
+
+interface NavData {
+  index: number;
+  level: number;
+  text: string;
+}
 
 interface NavProps {
   source: string;
 }
 
 const ArticleNav: React.FC<NavProps> = ({ source }) => {
-  const [list, setList] = useState<any[]>([]);
-  const [current, setCurrent] = useState('');
-  const [scrollY, setScrollY] = useState(0);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [position, setPosition] = useState<{ top: number; height: number }>({ top: 0, height: 0 });
+  const [list, setList] = useState<NavData[]>([]);
+  const [current, setCurrent] = useState<string>('');
+  const [scrollY, setScrollY] = useState<number>(0);
   const { scrollYProgress } = useScroll();
+  const ulRef = useRef<HTMLUListElement>(null);
   useMotionValueEvent(scrollYProgress, 'change', (latest) => {
     setScrollY(latest);
   });
@@ -20,9 +29,9 @@ const ArticleNav: React.FC<NavProps> = ({ source }) => {
     setCurrent(window.location.hash.replace('#', ''));
     const matchResult = getTitle(source);
     if (matchResult) {
-      const navData = matchResult.map((r, i) => ({
+      const navData: NavData[] = matchResult.map((r, i) => ({
         index: i,
-        level: r.match(/^#+/g)?.[0].length,
+        level: r.match(/^#+/g)?.[0].length || 0,
         text: r.replace(/#+\s(\S+)\n*/g, '$1'),
       }));
       setList(navData);
@@ -49,10 +58,36 @@ const ArticleNav: React.FC<NavProps> = ({ source }) => {
   }, [source]);
   return (
     <div className={classNames('transform-gpu hidden md:block  h-full w-36')}>
-      <ul className={classNames('sticky top-20 py-2 text-sm')}>
+      <ul ref={ulRef} className={classNames('sticky top-20 py-2 text-sm')}>
+        <AnimatePresence>
+          {hoveredIndex !== null && (
+            <motion.div
+              className='bg-muted -z-[1] rounded-2xl'
+              initial={{ top: position.top, height: position.height }}
+              animate={{ top: position.top, height: position.height }}
+              exit={{ opacity: 0 }}
+              style={{
+                position: 'absolute',
+                left: 0,
+                width: '100%',
+              }}
+              transition={{ type: 'spring', damping: 25, stiffness: 150 }}
+            />
+          )}
+        </AnimatePresence>
         <li className='pl-3'>{Math.floor(scrollY * 100)}%</li>
-        {list.map((nav) => (
+        {list.map((nav, index) => (
           <li
+            onMouseEnter={(e) => {
+              setHoveredIndex(index);
+              const liRect = e.currentTarget.getBoundingClientRect();
+              const ulRect = ulRef.current!.getBoundingClientRect();
+              setPosition({
+                top: liRect.top - ulRect.top + ulRef.current!.scrollTop,
+                height: liRect.height,
+              });
+            }}
+            onMouseLeave={() => setHoveredIndex(null)}
             aria-current={current === `heading-${nav.index}`}
             title={nav.text}
             onClick={() => {
@@ -61,7 +96,6 @@ const ArticleNav: React.FC<NavProps> = ({ source }) => {
             className={classNames(
               current === `heading-${nav.index}` && 'text-accent',
               'truncate',
-
             )}
             style={{ marginLeft: `${(nav.level - 1) * 10}px` }}
             key={nav.index}
