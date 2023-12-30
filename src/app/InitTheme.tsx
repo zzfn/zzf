d44bfafadd87b2032db832fb92b4c0f44e49e2eb
cc <44751363+zzfn@users.noreply.github.com>
2023-12-31 00:08:16 +0800

    feat: type

--- src/app/InitTheme.tsx
+++ src/app/InitTheme.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import type { ThemeProviderProps } from './types';
 import { useSetAtom,useAtom } from 'jotai';
 import { userAtom } from '../atoms/userAtoms';
@@ -13,11 +13,8 @@ import { searchAtom } from "../atoms/searchAtoms";
 const colorSchemes = ['light', 'dark'];
 const MEDIA = '(prefers-color-scheme: dark)';
 
-const defaultThemes = ['light', 'dark'];
-
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   storageKey = 'theme',
-  themes = defaultThemes,
   defaultTheme = 'system',
   attribute = 'data-color-mode',
   children,
@@ -35,7 +32,7 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
     function handleKeyDown(event: KeyboardEvent) {
       if ((event.metaKey || event.ctrlKey) && event.code === 'KeyK') {
         event.preventDefault();
-        setSearchVisible(true)
+        setSearchVisible(true);
       }
     }
 
@@ -72,20 +69,17 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
     d.style.colorScheme = colorSchemes.includes(resolved) ? resolved : fallback;
   }, []);
 
-  const setTheme = useCallback(
-    (theme: any) => {
-      const newTheme = typeof theme === 'function' ? theme(theme) : theme;
-      setThemeState(newTheme);
+  const setTheme = useCallback((theme: any) => {
+    const newTheme = typeof theme === 'function' ? theme(theme) : theme;
+    setThemeState(newTheme);
 
-      // Save to storage
-      try {
-        localStorage.setItem(storageKey, newTheme);
-      } catch (e) {
-        // Unsupported
-      }
-    },
-    [],
-  );
+    // Save to storage
+    try {
+      localStorage.setItem(storageKey, newTheme);
+    } catch (e) {
+      // Unsupported
+    }
+  }, []);
 
   const handleMediaQuery = useCallback(
     (e: MediaQueryListEvent | MediaQueryList) => {
@@ -96,15 +90,13 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
     [theme],
   );
 
-  // Always listen to System preference
   useEffect(() => {
     const media = window.matchMedia(MEDIA);
 
-    // Intentionally use deprecated listener methods to support iOS & old browsers
-    media.addEventListener('change',handleMediaQuery);
+    media.addEventListener('change', handleMediaQuery);
     handleMediaQuery(media);
 
-    return () => media.removeEventListener('change',handleMediaQuery);
+    return () => media.removeEventListener('change', handleMediaQuery);
   }, [handleMediaQuery]);
 
   // localStorage event handling
@@ -113,8 +105,6 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
       if (e.key !== storageKey) {
         return;
       }
-
-      // If default theme set, use it if localstorage === null (happens on local storage manual deletion)
       const theme = e.newValue || defaultTheme;
       setTheme(theme);
     };
@@ -133,61 +123,38 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   const { data } = useSearch({ keyword });
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
-    if(searchVisible){
-      setTimeout(()=>{
-        inputRef.current?.focus()
-      })
-    } else{
-      setKeyword('')
+    if (searchVisible) {
+      setTimeout(() => {
+        inputRef.current?.focus();
+      });
+    } else {
+      setKeyword('');
     }
-  },[searchVisible])
-  return (
-    <>
-      <Modal onCancel={()=>setSearchVisible(false)} visible={searchVisible}>
-        <div className='p-6'>
-        <Input ref={inputRef} onChange={handleInputChange} placeholder='elasticsearch强力驱动' />
-        <div className='h-[50vh] overflow-y-auto my-6'>
-          {data?.map((item) => (
-            <SearchArticleCard dataSource={item} key={item.id} />
-          ))}
-        </div>
-        </div>
-      </Modal>
-      <ThemeScript
-        {...{
-          storageKey,
-          themes,
-          defaultTheme,
-          attribute,
-          children,
-        }}
-      />
-      {children}
-    </>
-  );
-};
-
-const ThemeScript = memo(
-  ({
-     storageKey,
-     attribute,
-     defaultTheme,
-   }: ThemeProviderProps & { defaultTheme: string }) => {
-    const hasValidFallback = colorSchemes.includes(defaultTheme);
-    const scriptSrc = `!function(){try{
+  }, [searchVisible]);
+  const scriptSrc = useMemo(
+    () => `!function(){try{
       var d=document.documentElement,c='${attribute}',m=localStorage.getItem('${storageKey}');
       if(!m){m=window.matchMedia('${MEDIA}').matches?'dark':'light';}
       ${`d.style.colorScheme=(m==='light'||m==='dark')`}
       d.setAttribute(c, ${`(m||'')`});
-    }catch(e){}}();`;
-
-    return <script dangerouslySetInnerHTML={{ __html: scriptSrc }} />;
-  },
-  () => true,
-);
-
-
-// Helpers
+    }catch(e){}}();`,
+    [attribute, MEDIA],
+  );
+  return (
+    <>
+      <Modal onCancel={() => setSearchVisible(false)} visible={searchVisible}>
+        <div className='p-6'>
+          <Input ref={inputRef} onChange={handleInputChange} placeholder='elasticsearch强力驱动' />
+          <div className='h-[50vh] overflow-y-auto my-6'>
+            {data?.map((item) => <SearchArticleCard dataSource={item} key={item.id} />)}
+          </div>
+        </div>
+      </Modal>
+      <script dangerouslySetInnerHTML={{ __html: scriptSrc }} />
+      {children}
+    </>
+  );
+};
 const getTheme = (key: string, fallback?: string) => {
   let theme;
   try {
