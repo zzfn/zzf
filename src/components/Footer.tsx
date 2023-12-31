20da88ece569a3c4a7d6fa72c6b02867d0b2263e
cc <44751363+zzfn@users.noreply.github.com>
2024-01-01 00:07:13 +0800

    feat: nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Link from 'next/link';
 import OnlineCount from 'app/_components/OnlineCount';
 import Logo from './Logo';
+import locale_cn from 'dayjs/locale/zh-cn';
 
 const Footer = () => {
   return (
     <footer className='flex flex-col py-6 text-muted items-center gap-y-2 text-xs'>
-      Copyright&copy;2020-{dayjs().format('YYYY')}
+      Copyright&copy;2020-{dayjs().locale(locale_cn).format('YYYY')}
       <OnlineCount />
       <Link className='hover:underline' target='_blank' href='/api/feed.xml'>
         RSS feed
