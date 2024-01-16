d8a778102e017d1a083ebebca68d4e196d58ed35
cc <44751363+zzfn@users.noreply.github.com>
2024-01-16 23:37:58 +0800

    feat: 7

--- src/app/post/[id]/_components/Avatar.tsx
+++ src/app/post/[id]/_components/Avatar.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 
 const Avatar = function ({ userId = '' }: { userId: string | null; size?: number }) {
   const avatar = useMemo(() => {
-    return 'https://avatar.zzfzzf.com/6.x/bottts/svg?seed=' + userId;
+    return 'https://avatar.zzfzzf.com/7.x/bottts/svg?seed=' + userId;
   }, [userId]);
   return <Image className='w-10 h-10' width={40} height={40} src={avatar} alt={userId ?? ''} />;
 };
