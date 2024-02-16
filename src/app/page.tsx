efb53080bc969eda544c5b859a17a5429a13d94a
cc <44751363+zzfn@users.noreply.github.com>
2024-02-16 22:00:02 +0800

    feat: up version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Tooltip } from '@oc/design';
+import { IconButton, Tag, Tooltip } from '@oc/design';
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import { format } from 'utils/time';
@@ -30,8 +30,8 @@ async function getData() {
 export default async function Page() {
   const data = await getData();
   return (
-    <>
-      <div className='my-6 flex items-center gap-x-6'>
+    <div className='my-6 grid grid-cols-1 md:grid-cols-[auto_minmax(0,_calc(100%_-_296px_-_24px))]'>
+      <div className='flex flex-col items-center gap-y-6'>
         <Image
           priority={true}
           className='aspect-square rounded-full object-cover'
@@ -40,45 +40,42 @@ export default async function Page() {
           alt='avatar'
           src='https://w.zzfzzf.com/img/1707467164880.webp'
         />
-        <div className='flex flex-col gap-y-2'>
-          <h6 className='text-3xl font-bold'>Krupp</h6>
-          <p>遇见即是上上签</p>
-          <p>我爱摸鱼，身体好好</p>
+        <h6 className='text-3xl font-bold'>Krupp</h6>
+        <p>遇见即是上上签</p>
+      </div>
+      <div>
+        <div className='flex justify-between rounded'>
+          <h6 className='flex items-center text-xl text-accent'>
+            <IconLabel />
+            文章
+          </h6>
+          <Link href='/post'>
+            <IconButton>
+              <IconArrowForward />
+            </IconButton>
+          </Link>
+        </div>
+        <div className='grid md:grid-cols-2'>
+          {data.map((post: any) => (
+            <a
+              key={post.id}
+              href={`/post/${post.id}`}
+              className='hover:border-primary flex flex-col gap-y-1 rounded p-3 hover:bg-muted'
+            >
+              <div className='flex text-accent'>{post.title}</div>
+              <div className='flex items-center gap-x-2 text-xs text-muted'>
+                更新于
+                <Tooltip content={format(post.updatedAt)}>
+                  <time className='font-mono'>
+                    <TimeDiff time={post.updatedAt} />
+                  </time>
+                </Tooltip>
+                <span># {post.tag}</span>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
-      <div className='my-6 flex justify-between rounded'>
-        <h6 className='flex items-center text-xl text-accent'>
-          <IconLabel />
-          文章
-        </h6>
-        <Link href='/post'>
-          <IconButton>
-            <IconArrowForward />
-          </IconButton>
-        </Link>
-      </div>
-      <div className='grid md:grid-cols-2'>
-        {data.map((post: any) => (
-          <a
-            key={post.id}
-            href={`/post/${post.id}`}
-            className='hover:border-primary flex flex-col gap-y-1 rounded p-3 hover:bg-muted'
-          >
-            <div className='text-accent '>{post.title}</div>
-            <div className='flex items-center gap-x-2 text-xs text-muted'>
-              <time className='font-mono'>
-                <TimeDiff time={post.createdAt} />
-              </time>
-              {post.createdAt !== post.updatedAt && (
-                <Tooltip content={format(post.updatedAt)}>
-                  <span>（已编辑）</span>
-                </Tooltip>
-              )}
-              <span># {post.tag}</span>
-            </div>
-          </a>
-        ))}
-      </div>
-    </>
+    </div>
   );
 }
