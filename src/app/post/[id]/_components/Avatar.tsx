d8e36b069d54d2d3c76197c8a76d8744f0bf71c2
zzfn <44751363+zzfn@users.noreply.github.com>
2024-08-09 23:18:21 +0800

    feat: avatar

--- src/app/post/[id]/_components/Avatar.tsx
+++ src/app/post/[id]/_components/Avatar.tsx
@@ -1,10 +1,21 @@
 import Image from 'next/image';
 import { useMemo } from 'react';
+import { adventurer } from '@dicebear/collection';
+import { createAvatar } from '@dicebear/core';
 
-const Avatar = function ({ userId = '' }: { userId: string | null; size?: number }) {
-  const avatar = useMemo(() => {
-    return 'https://avatar.wwm.app/7.x/bottts/svg?seed=' + userId;
-  }, [userId]);
-  return <Image className='h-10 w-10' width={40} height={40} src={avatar} alt={userId ?? ''} />;
+const Avatar = function ({ userId = '' }: { userId: string; size?: number }) {
+  const avatar = createAvatar(adventurer, {
+    seed: userId,
+    flip: true,
+  });
+  return (
+    <Image
+      className='h-10 w-10'
+      width={40}
+      height={40}
+      src={avatar.toDataUri()}
+      alt={userId ?? ''}
+    />
+  );
 };
 export default Avatar;
