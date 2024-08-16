d669f64938e5b028a95c68018d7fda11fa46e7cd
cc <44751363+zzfn@users.noreply.github.com>
2024-08-16 23:23:45 +0800

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 import Navigation from './Navigation';
 import Menu from './Menu';
@@ -7,6 +8,9 @@ import { IconPersonAdd } from '@oc/icon';
 import { IconButton, Tooltip } from '@oc/design';
 import { cookies } from 'next/headers';
 import Image from 'next/image';
+import { useMotionValueEvent, useScroll } from 'framer-motion';
+import classNames from 'classnames';
+import { useState } from 'react';
 
 const navLinks = [
   { name: '文章', href: 'post' },
@@ -15,40 +19,32 @@ const navLinks = [
   { name: '心情', href: 'moments' },
 ];
 const Header = () => {
-  const cookieStore = cookies();
-
+  const { scrollY } = useScroll();
+  const [count, setCount] = useState(0);
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    setCount(latest);
+  });
   return (
-    <header className='fixed top-0 z-10 flex h-16 w-full items-center bg-opacity [backdrop-filter:saturate(180%)_blur(20px)]'>
-      <div className='container mx-auto flex items-center justify-between px-3'>
+    <header className={classNames('fixed top-0 z-10 flex h-16 w-full items-center')}>
+      <div
+        className={classNames(
+          'mx-auto flex items-center justify-between px-3 transition-all duration-1000',
+          count > 100
+            ? 'w-1/2 rounded-2xl border bg-neutral-muted [backdrop-filter:saturate(180%)_blur(20px)]'
+            : 'container',
+        )}
+      >
         <span className='text-2xl md:hidden'>
           <Menu navLinks={navLinks} />
         </span>
         <Link href='/'>
           <Logo width={75} height={25} />
         </Link>
-        <nav className='hidden items-center gap-x-4 rounded-2xl bg-neutral-muted px-6 py-2 font-medium md:flex'>
+        <nav className='hidden items-center gap-x-4 px-6 py-2 font-medium md:flex'>
           <Navigation navLinks={navLinks} />
         </nav>
         <span className='inline-flex items-center gap-x-2 text-2xl'>
           <GlobalSearch />
-
-          {cookieStore.has('username') ? (
-            <Image
-              className='rounded-full'
-              width={32}
-              height={32}
-              src={cookieStore.get('avatar_url')?.value || ''}
-              alt='user'
-            />
-          ) : (
-            <Tooltip content='login'>
-              <a href='https://github.com/login/oauth/authorize?client_id=966d19ff92ce135eeea3'>
-                <IconButton>
-                  <IconPersonAdd />
-                </IconButton>
-              </a>
-            </Tooltip>
-          )}
         </span>
       </div>
     </header>
