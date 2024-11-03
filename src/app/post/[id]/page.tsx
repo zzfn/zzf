2d709f780b1c657b942a428db3a3ce0356d518fd
cc <44751363+zzfn@users.noreply.github.com>
2024-11-03 20:40:34 +0800

    refactor(post): 移除未使用的 xss 导包

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -17,7 +17,6 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import ArticleNav from 'components/ArticleNav';
 import remarkGfm from 'remark-gfm';
-import xss from 'xss';
 import CodeSandpack from '../../_components/CodeSandpack';
 async function getData(id: string) {
   return fetchData<Article>({
