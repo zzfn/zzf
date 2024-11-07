02066b2c560292f86c7ea6a4a2ca2dc2887b8b6d
cc <44751363+zzfn@users.noreply.github.com>
2024-11-08 00:57:02 +0800

    chore: 删除中间件日志输出

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-import { geolocation, ipAddress } from '@vercel/functions';
-
-export function middleware(request: NextRequest) {
-  console.log(geolocation(request), ipAddress(request));
-}
