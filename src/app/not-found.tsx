e335a3028e32d99d6ca2f1b3f6c0f3f5409165ae
cc <44751363+zzfn@users.noreply.github.com>
2024-12-04 11:51:50 +0800

    build(deps): 更新 @oc/design 和 @oc/icon 的版本
    - 更新 @oc/design@1.0.3 的分辨率
    - 更新 @oc/icon@0.1.0 的分辨率

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { getCdn } from '../utils/getCdn';
 export default function NotFound() {
   return (
     <div className='flex items-center justify-center'>
-      <LottiePlayer size={500} url={getCdn('/assets/404.json')} />
+      {/*<LottiePlayer size={500} url={getCdn('/assets/404.json')} />*/}
     </div>
   );
 }
