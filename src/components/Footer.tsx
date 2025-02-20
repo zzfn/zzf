c5bd3f89d2a49dae16fca8952bbc5ebc0360270b
cc <44751363+zzfn@users.noreply.github.com>
2025-02-20 14:57:07 +0800

    fix: 更新 Footer 组件的服务状态链接
    更新 Footer 组件中的服务状态链接以指向新的 URL。这确保了用户能够访问正确的服务状态页面，提高了用户体验。
    
    -

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             <Link
               className='text-sm text-gray-600 transition-colors hover:text-purple-600 dark:text-gray-300 dark:hover:text-purple-400'
               target='_blank'
-              href='https://uptime.zzfzzf.com/status'
+              href='https://uptime.zzfzzf.com/status/m'
             >
               服务状态
             </Link>
