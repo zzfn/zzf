723fbd81b2d70988d67170af973451c5d357cc33
cc <44751363+zzfn@users.noreply.github.com>
2025-03-11 14:27:48 +0800

    chore(pnpm-lock.yaml, package.json, MdImage.tsx): 更新依赖并优化图片组件

--- src/app/post/[id]/_components/MdImage.tsx
+++ src/app/post/[id]/_components/MdImage.tsx
@@ -9,24 +9,25 @@ const MdImage = (props: any) => {
   return (
     <>
       <Image
-        className='m-0 cursor-zoom-in rounded-3xl transition-all hover:brightness-75'
+        className='m-0 w-full cursor-zoom-in rounded-3xl transition-all hover:brightness-75'
         sizes='100vw'
         loading='lazy'
         style={{
-          width: '100%',
+          maxWidth: '100%',
           objectFit: 'scale-down',
-          maxHeight: '200px',
+          maxHeight: '100%',
         }}
         width={4}
         height={3}
         onClick={() => setVisible(true)}
+        alt={props.alt}
         {...props}
       />
       <AnimatePresence>
         {visible && (
-          <Portal className='fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center backdrop-blur-md'>
+          <Portal className='fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center backdrop-blur-md'>
             <Image
-              className='bg-opacity max-h-[95vh] max-w-[95vw] cursor-zoom-out rounded rounded object-scale-down'
+              className='bg-opacity max-h-[95vh] max-w-[95vw] cursor-zoom-out rounded object-scale-down'
               width={4}
               height={3}
               style={{
@@ -34,6 +35,7 @@ const MdImage = (props: any) => {
                 objectFit: 'scale-down',
               }}
               onClick={() => setVisible(false)}
+              alt={props.alt}
               {...props}
             />
           </Portal>
